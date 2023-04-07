24f4f0b5f00d288e614d2cf7233d9beaa872a8ae
Ekaterina Loshakova <katyushka8905@yandex.ru>
2023-04-07 09:55:35 +0300

    hotfix

--- scripts/index.js
+++ scripts/index.js
@@ -44,32 +44,6 @@ const inputsEditProfile = formEditProfile.querySelectorAll(".form__field");
 const buttonSubmitAddCard = formAddCard.querySelector(".button-submit");
 const inputsAddCard = formAddCard.querySelectorAll(".form__field");
 
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
 /* Функция закрытия попапа по overlay */
 function closePopupOverlay(evt) {
   popups.forEach((popap) => {
